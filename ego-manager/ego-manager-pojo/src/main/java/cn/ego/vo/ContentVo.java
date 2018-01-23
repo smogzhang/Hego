d1b95b2a515acd49b135d477dd64bb1c1889d6f7
sully <15871443307@163.com>
2018-01-23 23:33:00 +0800

    根据内容分类分页展示详细内容信息

---
+++ ego-manager/ego-manager-pojo/src/main/java/cn/ego/vo/ContentVo.java
@@ -0,0 +1,95 @@
+package cn.ego.vo;
+
+import java.util.Date;
+
+/**
+ * 用于返回页面所要的内容信息
+ * 
+ * @author Sully
+ *
+ */
+public class ContentVo {
+
+	private long id;
+	private String title;
+	private String subTitle;
+	private String titleDesc;
+	private String url;
+	private String pic;
+	private String pic2;
+	private Date created;
+	private Date updated;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSubTitle() {
+		return subTitle;
+	}
+
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+
+	public String getTitleDesc() {
+		return titleDesc;
+	}
+
+	public void setTitleDesc(String titleDesc) {
+		this.titleDesc = titleDesc;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+	public String getPic2() {
+		return pic2;
+	}
+
+	public void setPic2(String pic2) {
+		this.pic2 = pic2;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+
+}
